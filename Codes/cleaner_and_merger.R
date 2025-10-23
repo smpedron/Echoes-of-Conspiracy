69e1342483f7f531642562d90c4e9ac3fad9fde6
Stephanie Pedron <pedronstephanie.sp@gmail.com>
2025-10-23 12:52:37 -0400

    Update cleaner_and_merger.R

--- Codes/cleaner_and_merger.R
+++ Codes/cleaner_and_merger.R
@@ -6,11 +6,14 @@ rm(list=ls())
 library(dplyr)
 library(magrittr)
 library(tidyverse)
+library(tidyr)
+library(stringdist)
 library(ggplot2)
 library(patchwork)
 library(scales)
 library(maps)
 library(usdata)
+library(readxl)
 
 
 setwd("C:/Users/steph/Documents/Courses/PhD/Research Projects/Conspiratorial Rhetoric (Miller)/DC Inbox Data")
@@ -82,6 +85,107 @@ dc_data <- dc_data %>%
 table(dc_data$trump_endorsed, dc_data$Party)
 
 rm(trump_endorsements)
+
+
+## BioGuide IDs
+library(yaml)
+curr <- yaml::read_yaml("https://raw.githubusercontent.com/unitedstates/congress-legislators/main/legislators-current.yaml")
+hist <- yaml::read_yaml("https://raw.githubusercontent.com/unitedstates/congress-legislators/main/legislators-historical.yaml")
+legislators <- c(curr, hist)
+leg_df <- lapply(legislators, function(x) {
+  data.frame(
+    First.Name = x$name$first,
+    Last.Name = x$name$last,
+    State = tail(x$terms, 1)[[1]]$state,
+    Bioguide = x$id$bioguide,
+    stringsAsFactors = FALSE)}) %>% 
+  bind_rows()
+
+## Cook PVI Data
+pvi <- read_excel("Cook PVI 1997-2025 v3.xlsx")
+
+pvi$State <- state.abb[match(pvi$State, state.name)]
+
+pvi <- pvi %>%
+  mutate(Cook_PVI = ifelse(Cook_PVI == "EVEN", "+0", Cook_PVI))
+
+pvi <- pvi %>%
+  mutate(Name = trimws(Name)) %>% 
+  mutate(Name = ifelse(Name == "", NA, Name)) %>%
+  separate(Name, into = c("Last.Name", "First.Name"),
+    sep = ",\\s*", fill = "right", extra = "merge") %>%
+  mutate(First.Name = trimws(First.Name))
+
+has_space <- grepl("\\s", pvi$Last.Name)
+split_names <- strsplit(pvi$Last.Name[has_space], "\\s+", perl = TRUE)
+pvi$First.Name[has_space] <- sapply(split_names, `[`, 1)
+pvi$Last.Name[has_space]  <- sapply(split_names, function(x) paste(x[-1], collapse = " "))
+rm(has_space, split_names)
+
+pvi_joined <- pvi %>%
+  left_join(leg_df %>% select(First.Name, Last.Name, State, Bioguide), by = c("First.Name", "Last.Name", "State"))
+pvi_joined <- pvi_joined %>%
+  mutate(
+    First.Name = ifelse(First.Name == "Scott" & Last.Name == "Franklin", "Franklin", First.Name),
+    Last.Name  = ifelse(First.Name == "Franklin" & Last.Name == "Franklin", "Scott", Last.Name))
+
+
+pvi_na <- pvi_joined %>%
+  filter(is.na(Bioguide))
+pvi_na_filled <- pvi_na %>%
+  select(-Bioguide) %>% 
+  left_join(leg_df %>% select(Last.Name, State, Bioguide), by = c("Last.Name", "State"))
+pvi_final <- pvi_joined %>%
+  filter(!is.na(Bioguide)) %>%   
+  bind_rows(pvi_na_filled) 
+sum(is.na(pvi_final$Bioguide)) # 208 still missing (issues because of names in PVI)
+
+
+## fuzzy pass
+pvi_missing <- pvi_final %>%
+  filter(is.na(Bioguide)) %>%
+  mutate(
+    Full.Name = tolower(paste(First.Name, Last.Name)),
+    State = toupper(trimws(State)))
+
+leg_df_norm <- leg_df %>%
+  mutate(
+    Full.Name = tolower(paste(First.Name, Last.Name)),
+    State = toupper(trimws(State)))
+
+find_closest_full <- function(full_name, state, leg_df){
+  candidates <- leg_df %>% filter(State == state)
+  if(nrow(candidates) == 0) return(data.frame(Bioguide = NA_character_))
+  distances <- stringdist(full_name, candidates$Full.Name, method = "jw")  # Jaro-Winkler
+  best_idx <- which.min(distances)
+  candidates[best_idx, "Bioguide", drop = FALSE]
+}
+
+closest_matches <- lapply(1:nrow(pvi_missing), function(i){
+  find_closest_full(pvi_missing$Full.Name[i], pvi_missing$State[i], leg_df_norm)
+})
+
+closest_matches_df <- bind_rows(closest_matches)
+pvi_missing$Bioguide <- closest_matches_df$Bioguide
+pvi_final <- pvi_final %>%
+  filter(!is.na(Bioguide)) %>%
+  bind_rows(pvi_missing) 
+
+sum(is.na(pvi_final$Bioguide)) 
+
+pvi_final <- pvi_final %>%
+  rename(BioGuide.ID = Bioguide,
+         Disctrict_PVI = District)
+
+
+## join into dc_data
+dc_data <- dc_data %>%
+  left_join(pvi_final %>%
+      select(Disctrict_PVI, Cook_PVI, Raw_Cook_PVI, BioGuide.ID), by = "BioGuide.ID")
+sum(is.na(dc_data$Cook_PVI)) # 709 NA (issues coming from PVI data)
+
+
+rm(list = setdiff(ls(), c("dc_data", "leg_df")))
 #
 
 ##### MERGE DATA #####
@@ -111,4 +215,7 @@ dc_data <- dc_data %>%
 
 rm(dw_nom, dw_nom_subset)
 
+
+## SAVING
+write.csv(dc_data, "dc_data_merged.csv", row.names = F)
 #
